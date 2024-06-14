48bf9bf2fa3dafa3f77f7d0a27143b8a3ce57857
Osman <osmangoni0827@gmail.com>
2024-06-14 10:58:09 +0600

    update

--- index.js
+++ index.js
@@ -37,77 +37,6 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    const database= await client.db("FationShoeDB")
-    const ShoeCollection=await database.collection("ShoeCollection")
-    const UserCollection=await database.collection("UserCollection");
-    app.get('/task', async(req,res)=>{
-       const {searchValue,searchCategory}=req.query;  
-       
-        const result= await ShoeCollection.find().toArray();
-       return res.send(result);
-    })
-    app.get('/task/:id', async(req,res)=>{
-        const id=req.params.id
-        const result= await ShoeCollection.findOne({_id: new ObjectId(id)});
-        res.send(result);
-    })
-    app.post('/create_task', async(req,res)=>{
-      
-        const data= await req.body;
-        console.log(data)
-        const result= await ShoeCollection.insertOne(data);
-        res.send(result);
-    })
-    
-    app.patch('/task/:id', async(req,res)=>{
-        const UpdateData=req.body;
-        const id= req.params.id;
-     
-        const result= await ShoeCollection.updateOne({_id: new ObjectId(id)},
-        {$set:UpdateData});
-        res.send(result);
-    })
-    app.delete('/task/:id', async(req,res)=>{
-        const id=req.params.id
-        const result= await ShoeCollection.deleteOne({_id: new ObjectId(id)});
-        res.send(result);
-    })
-    app.get('/user', async(req,res)=>{
-      const email=req.params.id
-      const result= await UserCollection.findOne({email: email});
-      res.send(result);
-  })
-  app.get('/user/:email', async(req,res)=>{
-    const email=req.params.email
-    const result= await UserCollection.findOne({email: email});
-    res.send(result);
-   
-})
-    app.post('/add_user', async(req,res)=>{
-      const newUser=req.body;
-     
-      const token=createToken(newUser);
-    
-      const isExistUser= await UserCollection.findOne({email:newUser?.email})
-      if(isExistUser){
-        return res.send({
-          status:"200",
-          message:"Successfully LoggedIn",
-          token
-        })
-      }
-     await UserCollection.insertOne(newUser);
-    return res.send({token})
-    })
-    app.patch('/user/:email',verifyToken, async(req,res)=>{
-      const UpdateData=req.body;
-      const email= req.params.email;
-      
-      const result= await UserCollection.updateOne({email:email},
-      {$set:UpdateData});
-    
-      res.send(result);
-  })
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -122,3 +51,4 @@ app.get('/', (req, res) => {
 app.listen(port,()=>{
         console.log("Server Run in port ",port)
 })
+
